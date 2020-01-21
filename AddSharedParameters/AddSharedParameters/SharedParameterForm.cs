25be7dd69ac58dfdc417bf4883258ceda1e23389
Will Kang <archipycom@gmail.com>
2020-01-21 23:21:03 +1300

    ExternalDefinition as keys and Dictionary as values

--- AddSharedParameters/AddSharedParameters/SharedParameterForm.cs
+++ AddSharedParameters/AddSharedParameters/SharedParameterForm.cs
@@ -26,22 +26,63 @@ namespace AddSharedParameters
 
             definitionfile = app.OpenSharedParameterFile();
 
-            GroupSelectComboBox.Items.AddRange(GetSharedParamDict().Values.Distinct().ToList().ToArray());
+            GroupSelectComboBox.Items.AddRange(GetGroupListFromDict().ToArray());
+            //GroupSelectComboBox.Items.AddRange(GetSharedParamDict().Values.Distinct().ToList().ToArray());
             ParameterList.Items.Add("Please select a group.");
             GroupParameterUnderComboBox.Items.AddRange(ParameterGroupUnderList(doc).Keys.ToArray());
             CategoryCheckList.Items.AddRange(ParameterCategoryList(doc).Keys.ToList().ToArray());
         }
 
+        private Dictionary<ExternalDefinition, Dictionary<string, string>> GetSharedParamDict()
+        {
+            Dictionary<ExternalDefinition, Dictionary<string, string>> paramDict = new Dictionary<ExternalDefinition, Dictionary<string, string>>();
+
+            foreach (DefinitionGroup definitionGroup in definitionfile.Groups)
+            {
+                string deGroupName = definitionGroup.Name;
+                foreach (Definition definition in definitionGroup.Definitions)
+                {
+                    ExternalDefinition exDefinitionParamName = definition as ExternalDefinition;
+                    string deParamName = definition.Name;
+                    Dictionary<string, string> tempDict = new Dictionary<string, string>();
+                    tempDict.Add(deParamName,deGroupName);
+
+                    if (!paramDict.ContainsKey(exDefinitionParamName))
+                    {
+                        paramDict.Add(exDefinitionParamName, tempDict);
+                    }
+                }
+            }
+            return paramDict;
+        }
+
+        private List<string> GetGroupListFromDict()
+        {
+            List<string> lst = new List<string>();
+            foreach (var dictPair in GetSharedParamDict())
+            {
+                foreach (var innerPair in dictPair.Value)
+                {
+                    if (!lst.Contains(innerPair.Value))
+                    {
+                        lst.Add(innerPair.Value);
+                    }
+                }
+            }
+            return lst;
+        }
+
+        /*
         private Dictionary<string, string> GetSharedParamDict()
         {
             Dictionary<string, string> paramDict = new Dictionary<string, string>();
 
             foreach (DefinitionGroup definitionGroup in definitionfile.Groups)
             {
-                string deGroupName = definitionGroup.Name.ToString();
+                string deGroupName = definitionGroup.Name;
                 foreach (Definition definition in definitionGroup.Definitions)
                 {
-                    string deParamName = definition.Name.ToString();
+                    string deParamName = definition.Name;
                     if (!paramDict.ContainsKey(deParamName))
                     {
                         paramDict.Add(deParamName, deGroupName);
@@ -50,7 +91,32 @@ namespace AddSharedParameters
             }
             return paramDict;
         }
+        */
+        /*
+        private Dictionary<string, string> GetSharedParamDictExternalDefinition()
+        {
+            Dictionary<string, string> paramDict = new Dictionary<string, string>();
 
+            DefinitionGroup definitionGroup = definitionfile.Groups.get_Item(GroupSelectComboBox.SelectedItem.ToString());
+
+            foreach(Definition de in definitionGroup.Definitions)
+            {
+                foreach (string pn in ParameterList.SelectedItems)
+                {
+                    Definition deSelected = null;
+                    if(deSelected.Name == pn)
+                    {
+                        
+                    }
+                    ExternalDefinition exParamName = 
+                }
+            }
+            
+
+
+            return paramDict;
+        }
+        */
         private SortedList<string, Category> ParameterCategoryList(Document doc)
         {
             Categories categories = doc.Settings.Categories;
@@ -126,83 +192,40 @@ namespace AddSharedParameters
             
             return validGroups;
         }
-        /*
-        private List<BuiltInParameterGroup> ParameterGroupUnderList(Document doc)
-        {
-            List<BuiltInParameterGroup> validGroups = new List<BuiltInParameterGroup>();
-            validGroups.Add(BuiltInParameterGroup.PG_ANALYSIS_RESULTS);
-            validGroups.Add(BuiltInParameterGroup.PG_ANALYTICAL_ALIGNMENT);
-            validGroups.Add(BuiltInParameterGroup.PG_ANALYTICAL_MODEL);
-            validGroups.Add(BuiltInParameterGroup.PG_CONSTRAINTS);
-            validGroups.Add(BuiltInParameterGroup.PG_CONSTRUCTION);
-            validGroups.Add(BuiltInParameterGroup.PG_DATA);
-            validGroups.Add(BuiltInParameterGroup.PG_GEOMETRY); // Dimensions
-            validGroups.Add(BuiltInParameterGroup.PG_DIVISION_GEOMETRY);
-            validGroups.Add(BuiltInParameterGroup.PG_AELECTRICAL);
-            validGroups.Add(BuiltInParameterGroup.PG_ELECTRICAL_CIRCUITING);
-            validGroups.Add(BuiltInParameterGroup.PG_ELECTRICAL_LIGHTING);
-            validGroups.Add(BuiltInParameterGroup.PG_ELECTRICAL_LOADS);
-            validGroups.Add(BuiltInParameterGroup.PG_ELECTRICAL);
-            validGroups.Add(BuiltInParameterGroup.PG_ENERGY_ANALYSIS);
-            validGroups.Add(BuiltInParameterGroup.PG_FIRE_PROTECTION);
-            validGroups.Add(BuiltInParameterGroup.PG_FORCES);
-            validGroups.Add(BuiltInParameterGroup.PG_GENERAL);
-            validGroups.Add(BuiltInParameterGroup.PG_GRAPHICS);
-            validGroups.Add(BuiltInParameterGroup.PG_GREEN_BUILDING);
-            validGroups.Add(BuiltInParameterGroup.PG_IDENTITY_DATA);
-            validGroups.Add(BuiltInParameterGroup.PG_IFC);
-            validGroups.Add(BuiltInParameterGroup.PG_REBAR_SYSTEM_LAYERS);
-            validGroups.Add(BuiltInParameterGroup.PG_MATERIALS);
-            validGroups.Add(BuiltInParameterGroup.PG_MECHANICAL);
-            validGroups.Add(BuiltInParameterGroup.PG_MECHANICAL_AIRFLOW);
-            validGroups.Add(BuiltInParameterGroup.PG_MECHANICAL_LOADS);
-            validGroups.Add(BuiltInParameterGroup.PG_ADSK_MODEL_PROPERTIES);
-            validGroups.Add(BuiltInParameterGroup.PG_MOMENTS);
-            validGroups.Add(BuiltInParameterGroup.INVALID); // other
-            validGroups.Add(BuiltInParameterGroup.PG_OVERALL_LEGEND);
-            validGroups.Add(BuiltInParameterGroup.PG_PHASING);
-            validGroups.Add(BuiltInParameterGroup.PG_LIGHT_PHOTOMETRICS);
-            validGroups.Add(BuiltInParameterGroup.PG_PLUMBING);
-            validGroups.Add(BuiltInParameterGroup.PG_PRIMARY_END);
-            validGroups.Add(BuiltInParameterGroup.PG_REBAR_ARRAY);
-            validGroups.Add(BuiltInParameterGroup.PG_RELEASES_MEMBER_FORCES);
-            validGroups.Add(BuiltInParameterGroup.PG_SECONDARY_END);
-            validGroups.Add(BuiltInParameterGroup.PG_SECONDARY_END);
-            validGroups.Add(BuiltInParameterGroup.PG_SEGMENTS_FITTINGS);
-            validGroups.Add(BuiltInParameterGroup.PG_SLAB_SHAPE_EDIT);
-            validGroups.Add(BuiltInParameterGroup.PG_STRUCTURAL);
-            validGroups.Add(BuiltInParameterGroup.PG_STRUCTURAL_ANALYSIS);
-            validGroups.Add(BuiltInParameterGroup.PG_TEXT);
-            validGroups.Add(BuiltInParameterGroup.PG_TITLE);
-            validGroups.Add(BuiltInParameterGroup.PG_VISIBILITY);
 
-            return validGroups;
-        }
-        */
         private void ParameterList_SelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
+
         private void AddButton_Click(object sender, EventArgs e)
         {
             
-        }
 
+        }
+        
         private void GroupSelectComboBox_SelectedIndexChanged(object sender, EventArgs e)
         {
             ParameterList.Items.Clear();
             SelectedGroup = GroupSelectComboBox.SelectedItem;
             string selGroup = SelectedGroup.ToString();
 
-            foreach (KeyValuePair<string, string> v in GetSharedParamDict())
+            foreach (var dictPair in GetSharedParamDict())
+            //foreach (KeyValuePair<string, string> v in GetSharedParamDict())
             {
-                if (v.Value == selGroup)
+                foreach (var innerPair in dictPair.Value)
                 {
-                    ParameterList.Items.Add(v.Key);
+                    if (innerPair.Value == selGroup)
+                    //if (v.Value == selGroup)
+                    {
+                        ParameterList.Items.Add(innerPair.Key);
+
+                    }
                 }
+
             }
         }
-
+        
         private void CancelButton_Click(object sender, EventArgs e)
         {
             this.Close();
