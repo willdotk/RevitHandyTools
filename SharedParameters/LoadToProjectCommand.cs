d48f309b3d8d49679d1bdaa05766eb3396948aaa
Will Kang <archipycom@gmail.com>
2020-02-09 02:36:56 +1300

    initialise RevitHandyTools and merge tools

---
+++ SharedParameters/LoadToProjectCommand.cs
@@ -0,0 +1,43 @@
+﻿#region Namespaces
+using System;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+#endregion
+
+namespace RevitHandyTools.SharedParameters
+{
+    [Transaction(TransactionMode.Manual)]
+    class LoadToProjectCommand : IExternalCommand
+    {
+        public Result Execute(
+          ExternalCommandData commandData,
+          ref string message,
+          ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Application app = uiapp.Application;
+            Document doc = uidoc.Document;
+
+            try
+            {
+                LoadToProjectForm addingForm = new LoadToProjectForm(doc, app);
+                addingForm.ShowDialog();
+
+                if (addingForm.DialogResult == System.Windows.Forms.DialogResult.OK)
+                {
+                    addingForm.AddParametersToProject(doc, app);
+                }
+
+                return Result.Succeeded;
+            }
+            catch (Exception e)
+            {
+                TaskDialog.Show("Error", e.ToString());
+                return Result.Failed;
+            }
+        }
+    }
+}
