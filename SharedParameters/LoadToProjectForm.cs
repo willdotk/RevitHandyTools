eecb6fff4c5705db47f8c305358e915a1a3b00ab
Will Kang <archipycom@gmail.com>
2020-02-10 00:17:38 +1300

    SharedParametersLibrary.cs added

--- SharedParameters/LoadToProjectForm.cs
+++ SharedParameters/LoadToProjectForm.cs
@@ -10,11 +10,11 @@ namespace RevitHandyTools.SharedParameters
 {
     public partial class LoadToProjectForm : System.Windows.Forms.Form
     {
-        private DefinitionFile definitionfile = null;
-        private Object SelectedGroup = null;
+        public DefinitionFile definitionfile = null;
+        public Object SelectedGroup = null;
 
-        private Autodesk.Revit.ApplicationServices.Application app = null;
-        private Document doc = null;
+        public Autodesk.Revit.ApplicationServices.Application app = null;
+        public Document doc = null;
 
         public LoadToProjectForm(Document dOcument, Autodesk.Revit.ApplicationServices.Application aPplication)
         {
@@ -25,136 +25,29 @@ namespace RevitHandyTools.SharedParameters
 
             definitionfile = app.OpenSharedParameterFile();
 
-            GroupSelectComboBox.Items.AddRange(GetGroupListFromDict().ToArray());
+            List<string> groupListDict = new SharedParametersLibrary(doc,app).GetGroupListFromDict();
+            Dictionary<string, BuiltInParameterGroup> paramGroupUnderDict = new SharedParametersLibrary(doc,app).ParameterGroupUnderDict(doc);
+            SortedList<string, Category> paramCategoryList = new SharedParametersLibrary(doc,app).ParameterCategoryList(doc);
+
+            GroupSelectComboBox.Items.AddRange(groupListDict.ToArray());
             ParameterList.Items.Add("Please select a group.");
-            GroupParameterUnderComboBox.Items.AddRange(ParameterGroupUnderDict(doc).Keys.ToArray());
-            CategoryCheckList.Items.AddRange(ParameterCategoryList(doc).Keys.ToList().ToArray());
+            GroupParameterUnderComboBox.Items.AddRange(paramGroupUnderDict.Keys.ToArray());
+            CategoryCheckList.Items.AddRange(paramCategoryList.Keys.ToList().ToArray());
             InstanceCheck.Checked = true;
-        }
-        private Dictionary<ExternalDefinition, Dictionary<string, string>> GetSharedParamDict()
-        {
-            Dictionary<ExternalDefinition, Dictionary<string, string>> paramDict = new Dictionary<ExternalDefinition, Dictionary<string, string>>();
-
-            foreach (DefinitionGroup definitionGroup in definitionfile.Groups)
-            {
-                string deGroupName = definitionGroup.Name;
-                foreach (Definition definition in definitionGroup.Definitions)
-                {
-                    ExternalDefinition exDefinitionParamName = definition as ExternalDefinition;
-                    string deParamName = definition.Name;
-                    Dictionary<string, string> tempDict = new Dictionary<string, string>();
-                    tempDict.Add(deParamName, deGroupName);
-
-                    if (!paramDict.ContainsKey(exDefinitionParamName))
-                    {
-                        paramDict.Add(exDefinitionParamName, tempDict);
-                    }
-                }
-            }
-            return paramDict;
-        }
-
-        private List<string> GetGroupListFromDict()
-        {
-            List<string> lst = new List<string>();
-            foreach (var dictPair in GetSharedParamDict())
-            {
-                foreach (var innerPair in dictPair.Value)
-                {
-                    if (!lst.Contains(innerPair.Value))
-                    {
-                        lst.Add(innerPair.Value);
-                    }
-                }
-            }
-            return lst;
-        }
-
-        private SortedList<string, Category> ParameterCategoryList(Document doc)
-        {
-            Categories categories = doc.Settings.Categories;
-
-            SortedList<string, Category> categoryList = new SortedList<string, Category>();
-
-            foreach (Category cat in categories)
-            {
-                if (cat.AllowsBoundParameters)
-                {
-                    categoryList.Add(cat.Name, cat);
-                }
-            }
-            return categoryList;
-        }
-
-        public static string FirstCharToUpper(string inputString)
-        {
-            TextInfo textInfo = new CultureInfo("en-US", false).TextInfo;
-            string removePG = Regex.Replace(inputString, @"PG_", "");
-            string lowerString = Regex.Replace(removePG, @"_", " ").ToLower();
-            string groupString = textInfo.ToTitleCase(lowerString);
-            return groupString;
-        }
-
-        private Dictionary<string, BuiltInParameterGroup> ParameterGroupUnderDict(Document doc)
-        {
-            Dictionary<string, BuiltInParameterGroup> validGroups = new Dictionary<string, BuiltInParameterGroup>();
-            validGroups.Add(FirstCharToUpper(BuiltInParameterGroup.PG_ANALYSIS_RESULTS.ToString()), BuiltInParameterGroup.PG_ANALYSIS_RESULTS);
-            validGroups.Add(FirstCharToUpper(BuiltInParameterGroup.PG_ANALYTICAL_ALIGNMENT.ToString()), BuiltInParameterGroup.PG_ANALYTICAL_ALIGNMENT);
-            validGroups.Add(FirstCharToUpper(BuiltInParameterGroup.PG_ANALYTICAL_MODEL.ToString()), BuiltInParameterGroup.PG_ANALYTICAL_MODEL);
-            validGroups.Add(FirstCharToUpper(BuiltInParameterGroup.PG_CONSTRAINTS.ToString()), BuiltInParameterGroup.PG_CONSTRAINTS);
-            validGroups.Add(FirstCharToUpper(BuiltInParameterGroup.PG_CONSTRUCTION.ToString()), BuiltInParameterGroup.PG_CONSTRUCTION);
-            validGroups.Add(FirstCharToUpper(BuiltInParameterGroup.PG_DATA.ToString()), BuiltInParameterGroup.PG_DATA);
-            validGroups.Add("Dimensions", BuiltInParameterGroup.PG_GEOMETRY); // Dimensions
-            validGroups.Add(FirstCharToUpper(BuiltInParameterGroup.PG_DIVISION_GEOMETRY.ToString()), BuiltInParameterGroup.PG_DIVISION_GEOMETRY);
-            validGroups.Add("Electrical", BuiltInParameterGroup.PG_AELECTRICAL);
-            validGroups.Add(FirstCharToUpper(BuiltInParameterGroup.PG_ELECTRICAL_CIRCUITING.ToString()), BuiltInParameterGroup.PG_ELECTRICAL_CIRCUITING);
-            validGroups.Add(FirstCharToUpper(BuiltInParameterGroup.PG_ELECTRICAL_LIGHTING.ToString()), BuiltInParameterGroup.PG_ELECTRICAL_LIGHTING);
-            validGroups.Add(FirstCharToUpper(BuiltInParameterGroup.PG_ELECTRICAL_LOADS.ToString()), BuiltInParameterGroup.PG_ELECTRICAL_LOADS);
-            validGroups.Add("Electrical Engineering", BuiltInParameterGroup.PG_ELECTRICAL);
-            validGroups.Add(FirstCharToUpper(BuiltInParameterGroup.PG_ENERGY_ANALYSIS.ToString()), BuiltInParameterGroup.PG_ENERGY_ANALYSIS);
-            validGroups.Add(FirstCharToUpper(BuiltInParameterGroup.PG_FIRE_PROTECTION.ToString()), BuiltInParameterGroup.PG_FIRE_PROTECTION);
-            validGroups.Add(FirstCharToUpper(BuiltInParameterGroup.PG_FORCES.ToString()), BuiltInParameterGroup.PG_FORCES);
-            validGroups.Add(FirstCharToUpper(BuiltInParameterGroup.PG_GENERAL.ToString()), BuiltInParameterGroup.PG_GENERAL);
-            validGroups.Add(FirstCharToUpper(BuiltInParameterGroup.PG_GRAPHICS.ToString()), BuiltInParameterGroup.PG_GRAPHICS);
-            validGroups.Add("Green Building Properties", BuiltInParameterGroup.PG_GREEN_BUILDING);
-            validGroups.Add(FirstCharToUpper(BuiltInParameterGroup.PG_IDENTITY_DATA.ToString()), BuiltInParameterGroup.PG_IDENTITY_DATA);
-            validGroups.Add("IFC Parameters", BuiltInParameterGroup.PG_IFC);
-            validGroups.Add("Layers", BuiltInParameterGroup.PG_REBAR_SYSTEM_LAYERS);
-            validGroups.Add("Materials and Finishes", BuiltInParameterGroup.PG_MATERIALS);
-            validGroups.Add(FirstCharToUpper(BuiltInParameterGroup.PG_MECHANICAL.ToString()), BuiltInParameterGroup.PG_MECHANICAL);
-            validGroups.Add("Mechanical - Flow", BuiltInParameterGroup.PG_MECHANICAL_AIRFLOW);
-            validGroups.Add(FirstCharToUpper(BuiltInParameterGroup.PG_MECHANICAL_LOADS.ToString()), BuiltInParameterGroup.PG_MECHANICAL_LOADS);
-            validGroups.Add("Model Properties", BuiltInParameterGroup.PG_ADSK_MODEL_PROPERTIES);
-            validGroups.Add(FirstCharToUpper(BuiltInParameterGroup.PG_MOMENTS.ToString()), BuiltInParameterGroup.PG_MOMENTS);
-            validGroups.Add("Other", BuiltInParameterGroup.INVALID); // other
-            validGroups.Add(FirstCharToUpper(BuiltInParameterGroup.PG_OVERALL_LEGEND.ToString()), BuiltInParameterGroup.PG_OVERALL_LEGEND);
-            validGroups.Add(FirstCharToUpper(BuiltInParameterGroup.PG_PHASING.ToString()), BuiltInParameterGroup.PG_PHASING);
-            validGroups.Add("Photometrics", BuiltInParameterGroup.PG_LIGHT_PHOTOMETRICS);
-            validGroups.Add(FirstCharToUpper(BuiltInParameterGroup.PG_PLUMBING.ToString()), BuiltInParameterGroup.PG_PLUMBING);
-            validGroups.Add(FirstCharToUpper(BuiltInParameterGroup.PG_PRIMARY_END.ToString()), BuiltInParameterGroup.PG_PRIMARY_END);
-            validGroups.Add("Rebar Set", BuiltInParameterGroup.PG_REBAR_ARRAY);
-            validGroups.Add("Releases / Member Forces", BuiltInParameterGroup.PG_RELEASES_MEMBER_FORCES);
-            validGroups.Add(FirstCharToUpper(BuiltInParameterGroup.PG_SECONDARY_END.ToString()), BuiltInParameterGroup.PG_SECONDARY_END);
-            validGroups.Add("Segments and Fittings", BuiltInParameterGroup.PG_SEGMENTS_FITTINGS);
-            validGroups.Add(FirstCharToUpper(BuiltInParameterGroup.PG_SLAB_SHAPE_EDIT.ToString()), BuiltInParameterGroup.PG_SLAB_SHAPE_EDIT);
-            validGroups.Add(FirstCharToUpper(BuiltInParameterGroup.PG_STRUCTURAL.ToString()), BuiltInParameterGroup.PG_STRUCTURAL);
-            validGroups.Add(FirstCharToUpper(BuiltInParameterGroup.PG_STRUCTURAL_ANALYSIS.ToString()), BuiltInParameterGroup.PG_STRUCTURAL_ANALYSIS);
-            validGroups.Add(FirstCharToUpper(BuiltInParameterGroup.PG_TEXT.ToString()), BuiltInParameterGroup.PG_TEXT);
-            validGroups.Add("Title Text", BuiltInParameterGroup.PG_TITLE);
-            validGroups.Add(FirstCharToUpper(BuiltInParameterGroup.PG_VISIBILITY.ToString()), BuiltInParameterGroup.PG_VISIBILITY);
-
-            return validGroups;
         }
 
         public void AddParametersToProject(Document doc, Autodesk.Revit.ApplicationServices.Application app)
         {
+            Dictionary<string, BuiltInParameterGroup> paramGroupUnderDict = new SharedParametersLibrary(doc,app).ParameterGroupUnderDict(doc);
+            Dictionary<ExternalDefinition, Dictionary<string, string>> sharedParamDict = new SharedParametersLibrary(doc,app).GetSharedParamDict();
+            SortedList<string, Category> paramCategoryList = new SharedParametersLibrary(doc,app).ParameterCategoryList(doc);
 
             CategorySet categoryset = app.Create.NewCategorySet();
 
             BuiltInParameterGroup parameterGroupUnder = new BuiltInParameterGroup();
             string selectedGroup = GroupParameterUnderComboBox.SelectedItem.ToString();
 
-            foreach (KeyValuePair<string, BuiltInParameterGroup> k in ParameterGroupUnderDict(doc))
+            foreach (KeyValuePair<string, BuiltInParameterGroup> k in paramGroupUnderDict)
             {
                 if (k.Key == selectedGroup)
                 {
@@ -170,14 +63,14 @@ namespace RevitHandyTools.SharedParameters
 
                 foreach (string selectedParameter in ParameterList.SelectedItems)
                 {
-                    foreach (var dictPair in GetSharedParamDict())
+                    foreach (var dictPair in sharedParamDict)
                     {
 
                         foreach (var innerPair in dictPair.Value)
                         {
                             if (innerPair.Key.Contains(selectedParameter))
                             {
-                                foreach (KeyValuePair<string, Category> k in ParameterCategoryList(doc))
+                                foreach (KeyValuePair<string, Category> k in paramCategoryList)
                                 {
 
                                     foreach (string catString in CategoryCheckList.CheckedItems)
@@ -212,11 +105,13 @@ namespace RevitHandyTools.SharedParameters
 
         private void GroupSelectComboBox_SelectedIndexChanged(object sender, EventArgs e)
         {
+            Dictionary<ExternalDefinition, Dictionary<string, string>> sharedParamDict = new SharedParametersLibrary(doc,app).GetSharedParamDict();
+
             ParameterList.Items.Clear();
             SelectedGroup = GroupSelectComboBox.SelectedItem;
             string selGroup = SelectedGroup.ToString();
 
-            foreach (var dictPair in GetSharedParamDict())
+            foreach (var dictPair in sharedParamDict)
             {
                 foreach (var innerPair in dictPair.Value)
                 {
---
+++ SharedParameters/SharedParametersLibrary.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Autodesk.Revit.DB;
+
+namespace RevitHandyTools.SharedParameters
+{
+    class SharedParametersLibrary
+    {
+        public DefinitionFile definitionfile = null;
+        public Autodesk.Revit.ApplicationServices.Application app = null;
+        public Document doc = null;
+        
+        public SharedParametersLibrary(Document dOcument, Autodesk.Revit.ApplicationServices.Application aPplication)
+        {
+            app = aPplication;
+            doc = dOcument;
+            definitionfile = app.OpenSharedParameterFile();
+        }
+
+        public Dictionary<ExternalDefinition, Dictionary<string, string>> GetSharedParamDict()
+        {
+            Dictionary<ExternalDefinition, Dictionary<string, string>> paramDict = new Dictionary<ExternalDefinition, Dictionary<string, string>>();
+
+            foreach (DefinitionGroup definitionGroup in definitionfile.Groups)
+            {
+                string deGroupName = definitionGroup.Name;
+                foreach (Definition definition in definitionGroup.Definitions)
+                {
+                    ExternalDefinition exDefinitionParamName = definition as ExternalDefinition;
+                    string deParamName = definition.Name;
+                    Dictionary<string, string> tempDict = new Dictionary<string, string>();
+                    tempDict.Add(deParamName, deGroupName);
+
+                    if (!paramDict.ContainsKey(exDefinitionParamName))
+                    {
+                        paramDict.Add(exDefinitionParamName, tempDict);
+                    }
+                }
+            }
+            return paramDict;
+        }
+
+        public List<string> GetGroupListFromDict()
+        {
+            List<string> lst = new List<string>();
+            foreach (var dictPair in GetSharedParamDict())
+            {
+                foreach (var innerPair in dictPair.Value)
+                {
+                    if (!lst.Contains(innerPair.Value))
+                    {
+                        lst.Add(innerPair.Value);
+                    }
+                }
+            }
+            return lst;
+        }
+
+        public SortedList<string, Category> ParameterCategoryList(Document doc)
+        {
+            Categories categories = doc.Settings.Categories;
+
+            SortedList<string, Category> categoryList = new SortedList<string, Category>();
+
+            foreach (Category cat in categories)
+            {
+                if (cat.AllowsBoundParameters)
+                {
+                    categoryList.Add(cat.Name, cat);
+                }
+            }
+            return categoryList;
+        }
+
+        public static string FirstCharToUpper(string inputString)
+        {
+            TextInfo textInfo = new CultureInfo("en-US", false).TextInfo;
+            string removePG = Regex.Replace(inputString, @"PG_", "");
+            string lowerString = Regex.Replace(removePG, @"_", " ").ToLower();
+            string groupString = textInfo.ToTitleCase(lowerString);
+            return groupString;
+        }
+
+        public Dictionary<string, BuiltInParameterGroup> ParameterGroupUnderDict(Document doc)
+        {
+            Dictionary<string, BuiltInParameterGroup> validGroups = new Dictionary<string, BuiltInParameterGroup>();
+            validGroups.Add(FirstCharToUpper(BuiltInParameterGroup.PG_ANALYSIS_RESULTS.ToString()), BuiltInParameterGroup.PG_ANALYSIS_RESULTS);
+            validGroups.Add(FirstCharToUpper(BuiltInParameterGroup.PG_ANALYTICAL_ALIGNMENT.ToString()), BuiltInParameterGroup.PG_ANALYTICAL_ALIGNMENT);
+            validGroups.Add(FirstCharToUpper(BuiltInParameterGroup.PG_ANALYTICAL_MODEL.ToString()), BuiltInParameterGroup.PG_ANALYTICAL_MODEL);
+            validGroups.Add(FirstCharToUpper(BuiltInParameterGroup.PG_CONSTRAINTS.ToString()), BuiltInParameterGroup.PG_CONSTRAINTS);
+            validGroups.Add(FirstCharToUpper(BuiltInParameterGroup.PG_CONSTRUCTION.ToString()), BuiltInParameterGroup.PG_CONSTRUCTION);
+            validGroups.Add(FirstCharToUpper(BuiltInParameterGroup.PG_DATA.ToString()), BuiltInParameterGroup.PG_DATA);
+            validGroups.Add("Dimensions", BuiltInParameterGroup.PG_GEOMETRY); // Dimensions
+            validGroups.Add(FirstCharToUpper(BuiltInParameterGroup.PG_DIVISION_GEOMETRY.ToString()), BuiltInParameterGroup.PG_DIVISION_GEOMETRY);
+            validGroups.Add("Electrical", BuiltInParameterGroup.PG_AELECTRICAL);
+            validGroups.Add(FirstCharToUpper(BuiltInParameterGroup.PG_ELECTRICAL_CIRCUITING.ToString()), BuiltInParameterGroup.PG_ELECTRICAL_CIRCUITING);
+            validGroups.Add(FirstCharToUpper(BuiltInParameterGroup.PG_ELECTRICAL_LIGHTING.ToString()), BuiltInParameterGroup.PG_ELECTRICAL_LIGHTING);
+            validGroups.Add(FirstCharToUpper(BuiltInParameterGroup.PG_ELECTRICAL_LOADS.ToString()), BuiltInParameterGroup.PG_ELECTRICAL_LOADS);
+            validGroups.Add("Electrical Engineering", BuiltInParameterGroup.PG_ELECTRICAL);
+            validGroups.Add(FirstCharToUpper(BuiltInParameterGroup.PG_ENERGY_ANALYSIS.ToString()), BuiltInParameterGroup.PG_ENERGY_ANALYSIS);
+            validGroups.Add(FirstCharToUpper(BuiltInParameterGroup.PG_FIRE_PROTECTION.ToString()), BuiltInParameterGroup.PG_FIRE_PROTECTION);
+            validGroups.Add(FirstCharToUpper(BuiltInParameterGroup.PG_FORCES.ToString()), BuiltInParameterGroup.PG_FORCES);
+            validGroups.Add(FirstCharToUpper(BuiltInParameterGroup.PG_GENERAL.ToString()), BuiltInParameterGroup.PG_GENERAL);
+            validGroups.Add(FirstCharToUpper(BuiltInParameterGroup.PG_GRAPHICS.ToString()), BuiltInParameterGroup.PG_GRAPHICS);
+            validGroups.Add("Green Building Properties", BuiltInParameterGroup.PG_GREEN_BUILDING);
+            validGroups.Add(FirstCharToUpper(BuiltInParameterGroup.PG_IDENTITY_DATA.ToString()), BuiltInParameterGroup.PG_IDENTITY_DATA);
+            validGroups.Add("IFC Parameters", BuiltInParameterGroup.PG_IFC);
+            validGroups.Add("Layers", BuiltInParameterGroup.PG_REBAR_SYSTEM_LAYERS);
+            validGroups.Add("Materials and Finishes", BuiltInParameterGroup.PG_MATERIALS);
+            validGroups.Add(FirstCharToUpper(BuiltInParameterGroup.PG_MECHANICAL.ToString()), BuiltInParameterGroup.PG_MECHANICAL);
+            validGroups.Add("Mechanical - Flow", BuiltInParameterGroup.PG_MECHANICAL_AIRFLOW);
+            validGroups.Add(FirstCharToUpper(BuiltInParameterGroup.PG_MECHANICAL_LOADS.ToString()), BuiltInParameterGroup.PG_MECHANICAL_LOADS);
+            validGroups.Add("Model Properties", BuiltInParameterGroup.PG_ADSK_MODEL_PROPERTIES);
+            validGroups.Add(FirstCharToUpper(BuiltInParameterGroup.PG_MOMENTS.ToString()), BuiltInParameterGroup.PG_MOMENTS);
+            validGroups.Add("Other", BuiltInParameterGroup.INVALID); // other
+            validGroups.Add(FirstCharToUpper(BuiltInParameterGroup.PG_OVERALL_LEGEND.ToString()), BuiltInParameterGroup.PG_OVERALL_LEGEND);
+            validGroups.Add(FirstCharToUpper(BuiltInParameterGroup.PG_PHASING.ToString()), BuiltInParameterGroup.PG_PHASING);
+            validGroups.Add("Photometrics", BuiltInParameterGroup.PG_LIGHT_PHOTOMETRICS);
+            validGroups.Add(FirstCharToUpper(BuiltInParameterGroup.PG_PLUMBING.ToString()), BuiltInParameterGroup.PG_PLUMBING);
+            validGroups.Add(FirstCharToUpper(BuiltInParameterGroup.PG_PRIMARY_END.ToString()), BuiltInParameterGroup.PG_PRIMARY_END);
+            validGroups.Add("Rebar Set", BuiltInParameterGroup.PG_REBAR_ARRAY);
+            validGroups.Add("Releases / Member Forces", BuiltInParameterGroup.PG_RELEASES_MEMBER_FORCES);
+            validGroups.Add(FirstCharToUpper(BuiltInParameterGroup.PG_SECONDARY_END.ToString()), BuiltInParameterGroup.PG_SECONDARY_END);
+            validGroups.Add("Segments and Fittings", BuiltInParameterGroup.PG_SEGMENTS_FITTINGS);
+            validGroups.Add(FirstCharToUpper(BuiltInParameterGroup.PG_SLAB_SHAPE_EDIT.ToString()), BuiltInParameterGroup.PG_SLAB_SHAPE_EDIT);
+            validGroups.Add(FirstCharToUpper(BuiltInParameterGroup.PG_STRUCTURAL.ToString()), BuiltInParameterGroup.PG_STRUCTURAL);
+            validGroups.Add(FirstCharToUpper(BuiltInParameterGroup.PG_STRUCTURAL_ANALYSIS.ToString()), BuiltInParameterGroup.PG_STRUCTURAL_ANALYSIS);
+            validGroups.Add(FirstCharToUpper(BuiltInParameterGroup.PG_TEXT.ToString()), BuiltInParameterGroup.PG_TEXT);
+            validGroups.Add("Title Text", BuiltInParameterGroup.PG_TITLE);
+            validGroups.Add(FirstCharToUpper(BuiltInParameterGroup.PG_VISIBILITY.ToString()), BuiltInParameterGroup.PG_VISIBILITY);
+
+            return validGroups;
+        }
+    }
+}
