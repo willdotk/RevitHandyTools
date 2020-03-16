695d394636bc0afbbd25ab5b848d7d411ac919d0
Will Kang <archipycom@gmail.com>
2020-03-16 22:29:02 +1300

    Warning message box added to TransmitModelCommand

--- Coordination/TransmitModelCommand.cs
+++ Coordination/TransmitModelCommand.cs
@@ -1,6 +1,7 @@
 ﻿#region Namespaces
 using System;
 using System.Collections.Generic;
+using System.Windows.Forms;
 using Autodesk.Revit.ApplicationServices;
 using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
@@ -20,83 +21,96 @@ namespace RevitHandyTools.Coordination
         {
             UIApplication uiapp = commandData.Application;
             UIDocument uidoc = uiapp.ActiveUIDocument;
-            Application app = uiapp.Application;
+            Autodesk.Revit.ApplicationServices.Application app = uiapp.Application;
             Document doc = uidoc.Document;
 
-            try
-            {
-                ICollection<ElementId> viewTemplateIdCollection = new List<ElementId>();
+            DialogResult dialogresult = MessageBox.Show("This will remove critical settings of the project." +
+                " Please make sure you run this extension in a coordiantion model." + " Do you want to preceed?",
+                "Cleaning Project for Model Transmit" , MessageBoxButtons.YesNo);
 
-                #region
-                // Go to 3D view and delete all views except 3D view
-                /*
-                RevitCommandId threeDViewcommandId = RevitCommandId.LookupPostableCommandId(PostableCommand.Default3DView);
+            if (dialogresult == DialogResult.Yes && uidoc.ActiveView.Name.StartsWith("{3D"))
+            {
+                try
+                {
+                    ICollection<ElementId> viewTemplateIdCollection = new List<ElementId>();
+
+                    #region
+                    // To delete all view templates
+
+                    FilteredElementCollector viewCollector = new FilteredElementCollector(doc);
+                    viewCollector.OfClass(typeof(Autodesk.Revit.DB.View));
+
+                    foreach (Autodesk.Revit.DB.View v in viewCollector)
+                    {
+                        if (v.ViewType.ToString() != "ProjectBrowser" && v.ViewType.ToString() != "SystemBrowser" && v.Name != "{3D}")
+                        {
+                            viewTemplateIdCollection.Add(v.Id);
+                        }
+                    }
+                    #endregion
+
+                    #region
+                    // To delete all project parameters
+                    BindingMap bindingMap = doc.ParameterBindings;
+
+                    FilteredElementCollector parameterElementCollector = new FilteredElementCollector(doc);
+                    parameterElementCollector.OfClass(typeof(ParameterElement));
+
+                    foreach (ParameterElement parameterElement in parameterElementCollector)
+                    {
+                        if (bindingMap.Contains(parameterElement.GetDefinition()))
+                        {
+                            viewTemplateIdCollection.Add(parameterElement.Id);
+                        }
+                    }
+                    #endregion
+
+                    #region
+                    // To purge a project
+
+                    string s_commandToDisable = "ID_PURGE_UNUSED";
+                    RevitCommandId s_commandId = RevitCommandId.LookupCommandId(s_commandToDisable);
+
+
+                    uiapp.PostCommand(s_commandId);
+
+                    #endregion
+
+                    /*
+                    using (Transaction tx = new Transaction(doc))
+                    {
+                        tx.Start("Cleaning up a project for transmit");
+                        doc.Delete(viewTemplateIdCollection);
+                        tx.Commit();
+                    }
                 
+                    TaskDialog.Show("Revit", "The current project is now ready for transmit");
+                    */
+                    return Result.Succeeded;
+                }
+                catch (Exception e)
+                {
+                    TaskDialog.Show("Error", e.ToString());
+                    return Result.Failed;
+                }
+            }
+            else if(dialogresult == DialogResult.Yes && !uidoc.ActiveView.Name.StartsWith("{3D"))
+            {
+                TaskDialog.Show("Reivt", "You will move to 3D view now. Please run this extension after moving to 3D view.");
+                #region
+                RevitCommandId threeDViewcommandId = RevitCommandId.LookupPostableCommandId(PostableCommand.Default3DView);
+
                 if (commandData.Application.CanPostCommand(threeDViewcommandId))
                 {
                     commandData.Application.PostCommand(threeDViewcommandId);
                 }
-                */
                 #endregion
-
-                #region
-                // To delete all view templates
-
-                FilteredElementCollector viewCollector = new FilteredElementCollector(doc);
-                viewCollector.OfClass(typeof(View));
-
-                foreach (View v in viewCollector)
-                {
-                    if (v.ViewType.ToString() != "ProjectBrowser" && v.ViewType.ToString() != "SystemBrowser" && v.Name != "{3D}")
-                    {
-                        viewTemplateIdCollection.Add(v.Id);
-                    }
-                }
-                #endregion
-
-                #region
-                // To delete all project parameters
-                BindingMap bindingMap = doc.ParameterBindings;
-
-                FilteredElementCollector parameterElementCollector = new FilteredElementCollector(doc);
-                parameterElementCollector.OfClass(typeof(ParameterElement));
-
-                foreach (ParameterElement parameterElement in parameterElementCollector)
-                {
-                    if (bindingMap.Contains(parameterElement.GetDefinition()))
-                    {
-                        viewTemplateIdCollection.Add(parameterElement.Id);
-                    }
-                }
-                #endregion
-
-                #region
-                // To purge a project
- 
-                string s_commandToDisable = "ID_PURGE_UNUSED";
-                RevitCommandId s_commandId = RevitCommandId.LookupCommandId(s_commandToDisable);
-
-
-                uiapp.PostCommand(s_commandId);
-
-                #endregion
-
-                
-                using (Transaction tx = new Transaction(doc))
-                {
-                    tx.Start("Cleaning up a project for transmit");
-                    doc.Delete(viewTemplateIdCollection);
-                    tx.Commit();
-                }
-                
-                TaskDialog.Show("Revit", "The current project is now ready for transmit");
-                
                 return Result.Succeeded;
             }
-            catch (Exception e)
+            else
             {
-                TaskDialog.Show("Error", e.ToString());
-                return Result.Failed;
+                
+                return Result.Succeeded;
             }
         }
     }
