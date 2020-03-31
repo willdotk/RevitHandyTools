ec49326244e134ff224b3749600bc55e8484a850
Will Kang <archipycom@gmail.com>
2020-03-31 21:42:43 +1300

    build file update

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("RevitHandyTools")]
 [assembly: AssemblyDescription("Revit Add-In Description for RevitHandyTools")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Autodesk Inc.")]
+[assembly: AssemblyCompany("Archipy")]
 [assembly: AssemblyProduct("RevitHandyTools Revit C# .NET Add-In")]
-[assembly: AssemblyCopyright("Copyright 2019 (C) Jeremy Tammik, Autodesk Inc.")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2020.0.0.0")]
-[assembly: AssemblyFileVersion("2020.0.0.0")]
+[assembly: AssemblyVersion("2020.3.0.0")]
+[assembly: AssemblyFileVersion("2020.3.0.0")]
